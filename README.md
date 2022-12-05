74b4ff0a74cbe5a2dcfb4c1d58de0f9101bff1a4
Frosty648 <frosty.mp4@gmail.com>
2022-12-05 21:53:18 +0000

    Initial Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+KitPVP Based
+-------------
+A port of the framework on my minecraft server that was written with a datapack in java using a spigot plugin.
---
+++ src/main/java/team/frosty/superpug/kitpvpbased/KitpvpBased.java
@@ -0,0 +1,17 @@
+package team.frosty.superpug.kitpvpbased;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+// Fire
+public final class KitpvpBased extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+
+    }
+
+    @Override
+    public void onDisable() {
+
+    }
+}
