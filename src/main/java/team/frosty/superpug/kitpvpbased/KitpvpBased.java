cac3077ed5a995f2a5656cfc2f6eae1b9607f7aa
BuildTools <unconfigured@null.spigotmc.org>
2022-12-12 15:33:09 +0000

    changed stuff i would write more but i'm DYING

--- src/main/java/team/frosty/superpug/kitpvpbased/KitpvpBased.java
+++ src/main/java/team/frosty/superpug/kitpvpbased/KitpvpBased.java
@@ -4,15 +4,21 @@ import org.bukkit.plugin.java.JavaPlugin;
 
 // Fire
 public final class KitpvpBased extends JavaPlugin {
+    private static KitpvpBased instance;
 
     @Override
     public void onEnable() {
         getServer().getPluginManager().registerEvents(new Chat(), this);
         getServer().getPluginManager().registerEvents(new TeleBow(), this);
+        instance = this;
     }
 
     @Override
     public void onDisable() {
 
+    }
+
+    public static KitpvpBased getInstance(){
+        return instance;
     }
 }
