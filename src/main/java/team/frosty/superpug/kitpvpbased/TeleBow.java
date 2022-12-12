68823126902d49d8b24ad81a7c7c3678f3e087ad
BuildTools <unconfigured@null.spigotmc.org>
2022-12-12 15:33:28 +0000

    changed stuff i would write more but i'm DYING 2

--- src/main/java/team/frosty/superpug/kitpvpbased/TeleBow.java
+++ src/main/java/team/frosty/superpug/kitpvpbased/TeleBow.java
@@ -1,23 +1,28 @@
 package team.frosty.superpug.kitpvpbased;
-import org.bukkit.Bukkit;
-import org.bukkit.Location;
+import net.md_5.bungee.api.ChatMessageType;
+import net.md_5.bungee.api.chat.TextComponent;
+import org.bukkit.*;
 import org.bukkit.entity.Player;
 import org.bukkit.event.Listener;
 import java.util.HashMap;
+import java.util.UUID;
+
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.entity.EntityShootBowEvent;
 import org.bukkit.event.entity.ProjectileHitEvent;
+import org.bukkit.NamespacedKey;
+import org.bukkit.persistence.PersistentDataType;
 
 
 public class TeleBow implements Listener {
-
     //TODO add projectile trail to arrows for player firing
+    //|| !event.getBow().getItemMeta().getPersistentDataContainer().has(new NamespacedKey(KitpvpBased.getInstance(), "telebow"), PersistentDataType.STRING)
+
 
     //hashmap of the ID of the arrow fired by the player and the Display name of the firing player
-    HashMap<Integer,String> arrows = new HashMap<>();
+    HashMap<Integer, UUID> arrows = new HashMap<>();
     //hashmap of the Display name of the player that shot and unix timestamp they shot at
-    HashMap<String,Long> cooldown = new HashMap<>();
-
+    HashMap<UUID,Long> cooldown = new HashMap<>();
 
     @EventHandler
     public void onPlayerShoot(EntityShootBowEvent event){
@@ -27,13 +32,13 @@ public class TeleBow implements Listener {
         }
 
         //checks if the cooldown if expired before updating the hashmaps
-        if(checkcooldown(((Player) event.getEntity()).getDisplayName())){
+        if(checkcooldown(event.getEntity().getUniqueId(),(Player) event.getEntity())){
 
             //adds the ID of the shot arrow and display name of shooter to hashmap
-            arrows.put(event.getProjectile().getEntityId(),((Player) event.getEntity()).getDisplayName());
+            arrows.put(event.getProjectile().getEntityId(),event.getEntity().getUniqueId());
 
             //adds the display name of shooter and current unix timestamp to hashmap
-            cooldown.put(((Player) event.getEntity()).getDisplayName(),System.currentTimeMillis());
+            cooldown.put(event.getEntity().getUniqueId(),System.currentTimeMillis());
         }
     }
 
@@ -48,24 +53,24 @@ public class TeleBow implements Listener {
         //TODO change to single line or move inside player.teleport
         Location arrowpos = event.getHitBlock().getLocation();
         arrowpos.setY(arrowpos.getY() +2);
+        arrowpos.setDirection(player.getLocation().getDirection());
 
 
         player.teleport(arrowpos);
     }
 
 
-    public boolean checkcooldown(String DisplayName){
+    public boolean checkcooldown(UUID uuid, Player p){
         //if the player has not shot the telebow before (name not in cooldown hashmap) adds them to it and returns true
-        if(cooldown.get(DisplayName) == null){
+        if(cooldown.get(uuid) == null){
             return true;
         }
         //checks if the current unix timestamp - the cooldown has a difference of greater than 3000ms (3s)
-        if(System.currentTimeMillis() - cooldown.get(DisplayName)  >3000){
-            Bukkit.broadcastMessage("cooldown expired");
+        if(System.currentTimeMillis() - cooldown.get(uuid)  >3000){
             return true;
         }
         //returns false if cooldown if not expired
-        Bukkit.broadcastMessage("cooldown not expired");
+        p.spigot().sendMessage(ChatMessageType.ACTION_BAR, TextComponent.fromLegacyText(ChatColor.BOLD + "" + ChatColor.RED + "cooldown not expired"));
         return false;
     }
 }
