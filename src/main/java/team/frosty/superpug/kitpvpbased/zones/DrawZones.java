eed5cd3df1903253900b102dd68ab4cb82b5a6f7
Frosty648 <frosty.mp4@gmail.com>
2022-12-13 21:04:57 +0000

    new particle

--- src/main/java/team/frosty/superpug/kitpvpbased/zones/DrawZones.java
+++ src/main/java/team/frosty/superpug/kitpvpbased/zones/DrawZones.java
@@ -13,7 +13,7 @@ public class DrawZones extends BukkitRunnable{
                     Location particleLoc = new Location(l.getWorld(), l.getX(), l.getY(), l.getZ());
                     particleLoc.setX(l.getX() + Math.cos(d) * ZoneCheck.zonesize);
                     particleLoc.setZ(l.getZ() + Math.sin(d) * ZoneCheck.zonesize);
-                    l.getWorld().spawnParticle(Particle.REDSTONE, particleLoc, 1, new Particle.DustOptions(Color.WHITE, 5));
+                    l.getWorld().spawnParticle(Particle.CRIT, particleLoc, 3);
                 }
             }
         }
