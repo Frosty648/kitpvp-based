3117cd8ecd844eca18067018da2716533e218d8b
BuildTools <unconfigured@null.spigotmc.org>
2022-12-15 16:01:58 +0000

    updated zone class to functional parity with previous system

--- src/main/java/team/frosty/superpug/kitpvpbased/zones/Zone.java
+++ src/main/java/team/frosty/superpug/kitpvpbased/zones/Zone.java
@@ -2,21 +2,68 @@ package team.frosty.superpug.kitpvpbased.zones;
 
 import org.bukkit.Bukkit;
 import org.bukkit.Location;
+import org.bukkit.Particle;
 import org.bukkit.entity.Player;
+import org.bukkit.scheduler.BukkitRunnable;
+import team.frosty.superpug.kitpvpbased.KitpvpBased;
 
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.HashMap;
 
-public class Zone {
+public class Zone extends BukkitRunnable{
     private Location zoneLocation;
     private ArrayList<Double> Distances;
     private int charge;
-
+    private int chargeRate = 1;
 
     public Zone(Location l){
-        zoneLocation = l;
+        this.zoneLocation = l;
+        this.runTaskTimer(KitpvpBased.getInstance(), 0,5);
+    }
+
+    public Zone(Location l, Integer chargeRate){
+        this.zoneLocation = l;
+        this.chargeRate = chargeRate;
+        this.runTaskTimer(KitpvpBased.getInstance(), 0,5);
+    }
+
+    @Override
+    public void run(){
+        drawZone();
+
+
+        if(shouldCharge()){
+            increaseCharge(20);
+        }
+
+        if(this.charge >= 100){
+            this.cancel();
+            Bukkit.broadcastMessage("zone finished");
+        }
+
+
+
+    }
+
+
+    public void drawZone(){
+        for (int d = 0; d <= 90; d += 1) {
+            Location particleLoc = new Location(zoneLocation.getWorld(), zoneLocation.getX(), zoneLocation.getY(), zoneLocation.getZ());
+            particleLoc.setX(zoneLocation.getX() + Math.cos(d) * ZoneCheck.zoneSize);
+            particleLoc.setZ(zoneLocation.getZ() + Math.sin(d) * ZoneCheck.zoneSize);
+            zoneLocation.getWorld().spawnParticle(Particle.CRIT, particleLoc, 1, 0d, 0d, 0d, 0d);
+        }
+    }
+
+    public boolean shouldCharge(){
+        for (Player p : Bukkit.getOnlinePlayers()){
+            if(p.getLocation().distanceSquared(zoneLocation) < 7){
+                return true;
+            }
+        }
+        return false;
     }
 
     public void increaseCharge(int amount){
